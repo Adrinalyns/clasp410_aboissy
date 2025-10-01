9cbfa8d415c5b856e8dcce5659a81fe2fde7f733
Adrien Boissy <adrienb@0587455221.wireless.umich.net>
2025-10-01 17:50:49 -0400

    Finishing the code for question2, adding comments and docstring for each functions. Beginning to work on question3

--- Labs/Lab02/lab02.py
+++ Labs/Lab02/lab02.py
@@ -166,6 +166,11 @@ def solve_rk8(func, N1_init=.5, N2_init=.5, dt=10, t_final=100.0, a=1, b=2, c=1,
 
 
 def validate_model():
+    """
+    This function validate the Euler, RK8 solver, and the functions that calculates the derivatives
+    by plotting the results obtained with the same parameters as in figure 1 (the example given in the HW)
+    """
+    
     a=1
     b=2
     c=1
@@ -217,6 +222,11 @@ def validate_model():
     ax2.legend()
     
 def question1():
+    """
+    This function answers question 1 by plotting the results of both solvers with different step times.
+    We use the same situation as in Figure 1, and we change the step time, to see to what extent it 
+    influences the results for both methods.
+    """
     a=1
     b=2
     c=1
@@ -230,14 +240,17 @@ def question1():
     dt_array=[0.05,0.5,1,2]
     for dt in dt_array:
         
+        #Calculating the results for the 2 Methods
+
             #Euler method
         time_comp,N1_comp,N2_comp=euler_solve(dNdt_comp, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
             #RK8 method
         time_rk8_comp,N1_rk8_comp,N2_rk8_comp=solve_rk8(dNdt_comp, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
 
-        print(f'For dt = {dt} years, the Euler solution took {time_comp.size} iterations.')
-        print(f'For dt = {dt} years, the RK8 solution took {time_rk8_comp.size} iterations.')
+        #print(f'For dt = {dt} years, the Euler solution took {time_comp.size} iterations.')
+        #print(f'For dt = {dt} years, the RK8 solution took {time_rk8_comp.size} iterations.')
 
+        #plotting figures
         plt.figure()    #create a new figure for next plot
 
         plt.plot(time_comp,N1_comp,label=f'N1 with Euler')
@@ -257,11 +270,15 @@ def question1():
     dt_array=[0.001,0.01,0.05,0.09]
     for dt in dt_array:
 
+        #Calculating the results for the 2 Methods
+
             #Euler method
         time_PP,N1_PP,N2_PP=euler_solve(dNdt_predator_prey, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
             
             #RK8 method
         time_rk8_PP,N1_rk8_PP,N2_rk8_PP=solve_rk8(dNdt_predator_prey, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
+
+        #Plotting figures
 
         plt.figure() #create a new figure for next plot
 
@@ -276,27 +293,62 @@ def question1():
         plt.ylabel(r'$\frac{Population}{carrying-cap}$')
         plt.legend()
     
+def question2():
+    """
+    This function plots all the equilibrium states I have found. 
+    Thus it allows to answer question2, and highlights the influence 
+    of initial conditions and parameters on the behavior of both populations
+    """
+    
+    dt=1.
+    t_final=100.
 
+    #this array contains every equilibrium state that I found.
+    # each equilibrium is represented by an array containing the values of:
+    # [a,b,c,d,N1_init,N2_init]
+    equilibriums=[[1,1,1,1,0.2,0.8],[1,1,1,1,0.8,0.8],[1,1,1,1,0.2,0.2],[1,1,1,1,0.2,0.5],[1,1,1,1,0.8,0.5],[1,2,1,5,0.15,0.6],[1,2,1,7,0.1,0.6],[1,3,1,2,0.6,0.3],[1,2,1,3,0.3,0.6]]
+
+    for equilibrium in equilibriums:
+        #Settings for each equilibrium
+        a=equilibrium[0]
+        b=equilibrium[1]
+        c=equilibrium[2]
+        d=equilibrium[3]
+        N1_init=equilibrium[4]
+        N2_init=equilibrium[5]
+
+        #Calculating each equilibrium
+        time,N1,N2=solve_rk8(dNdt_comp, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
+
+        #Plotting each equilibrium
+        plt.figure()    #create a new figure for next plot
+        plt.plot(time,N1, label=f'N1 with RK8',linestyle='--')
+        plt.plot(time,N2, label=f'N2 with RK8',linestyle='--')
+        plt.title(f"Lokta Volterra Competition model\n dt={dt} year a={a}, b={b}, c={c}, d={d}, N1(0)={N1_init}, N2(0)={N2_init}")
+        plt.xlabel("Time (years)")
+        plt.ylabel(r'$\frac{Population}{carrying-cap}$')
+        plt.legend()
+
+dt=0.1
 a=1
 b=2
 c=1
-d=5
+d=3
 N1_init=0.3
-N2_init=0.5
-dt=1
-
+N2_init=0.6
 t_final=100
+#Calculating each equilibrium
+time,N1,N2=solve_rk8(dNdt_predator_prey, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
 
+#Plotting phase diagram
+plt.figure()
+plt.plot(N1,N2)
 
-time_rk8_comp,N1_rk8_comp,N2_rk8_comp=solve_rk8(dNdt_comp, N1_init=N1_init, N2_init=N2_init, dt=dt, t_final=t_final,a=a,b=b,c=c,d=d)
+#Plotting each equilibrium
 plt.figure()    #create a new figure for next plot
-
-
-plt.plot(time_rk8_comp,N1_rk8_comp, label=f'N1 with RK8',linestyle='--')
-plt.plot(time_rk8_comp,N2_rk8_comp, label=f'N2 with RK8',linestyle='--')
-plt.title(f"Lokta Volterra Competition model dt = {dt} year")
+plt.plot(time,N1, label=f'N1 with RK8',linestyle='--')
+plt.plot(time,N2, label=f'N2 with RK8',linestyle='--')
+plt.title(f"Lokta Volterra Competition model\n dt={dt} year a={a}, b={b}, c={c}, d={d}, N1(0)={N1_init}, N2(0)={N2_init}")
 plt.xlabel("Time (years)")
 plt.ylabel(r'$\frac{Population}{carrying-cap}$')
 plt.legend()
-
-    
